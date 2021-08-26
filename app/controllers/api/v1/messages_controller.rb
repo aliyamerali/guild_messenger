ca3154a122db9c51a9b98ff6a808b05f70221a2e
Aliya Merali <merali4@gmail.com>
2021-08-26 17:12:36 -0600

    Add happy path functionality for get last 100 messages from given sender

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,4 +1,19 @@
 class Api::V1::MessagesController < ApplicationController
+  def index
+    recipient_id = params[:recipient_id]
+    sender_id = params[:sender_id]
+    limit = params[:limit]
+
+    if recipient_id && sender_id && limit
+      messages = Message.where(recipient_id: recipient_id, sender_id: sender_id)
+               .order('created_at DESC')
+               .limit(limit)
+      render json: MessageSerializer.new(messages)
+    else
+      render json: { response: 'Bad Request'}, status: :bad_request
+    end
+  end
+
   def create
     new_message = Message.create(message_params)
     if new_message.save
--- spec/api/v1/requests/get_messages_spec.rb
+++ spec/api/v1/requests/get_messages_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'Get User\'s Messages Endpoint' do
 
       # 75 messages from sender A within 120 days
       75.times do |index|
-        Message.create(
+        Message.create!(
                       recipient_id: @recipient_id,
                       sender_id: @sender_a_id,
                       content: "Message #{index}",
@@ -19,7 +19,7 @@ RSpec.describe 'Get User\'s Messages Endpoint' do
 
       # 25 messages from sender A within 60 days
       25.times do |index|
-        Message.create(
+        Message.create!(
                       recipient_id: @recipient_id,
                       sender_id: @sender_a_id,
                       content: "Message #{index}",
@@ -29,7 +29,7 @@ RSpec.describe 'Get User\'s Messages Endpoint' do
 
       # 25 messages from sender A within 30 days
       25.times do |index|
-        Message.create(
+        Message.create!(
                       recipient_id: @recipient_id,
                       sender_id: @sender_a_id,
                       content: "Message #{index}",
@@ -39,7 +39,7 @@ RSpec.describe 'Get User\'s Messages Endpoint' do
 
       # 25 messages from sender B within 30 days
       50.times do |index|
-        Message.create(
+        Message.create!(
                       recipient_id: @recipient_id,
                       sender_id: @sender_b_id,
                       content: "Message #{index}",
@@ -47,29 +47,27 @@ RSpec.describe 'Get User\'s Messages Endpoint' do
                       )
       end
 
-      # stub out Time.now to return 8/20/21
-      allow(Time).to receive(:now) do
-        DateTime.new(2021,8,20)
-      end
+      # # stub out Time.now to return 8/20/21
+      # allow(Time).to receive(:now) do
+      #   DateTime.new(2021,8,20)
+      # end
     end
 
     it 'returns recipient\'s last 100 messages from a given sender' do
       limit = "100"
-      body =  {"sender_id": @sender_a_id, "recipient_id": @recipient_id, "limit": limit}
-
-      get '/api/v1/messages', params: body, as: :json
+      get "/api/v1/messages?recipient_id=#{@recipient_id}&sender_id=#{@sender_a_id}&limit=#{limit}"
 
       output = JSON.parse(response.body, symbolize_names: true)
 
       expect(response).to be_successful
       expect(output[:data].length).to eq(100)
       expect(output[:data].first[:type]).to eq("message")
-      expect(output[:data].first[:sender_id]).to eq(sender_a_id)
-      expect(output[:data].first[:created_at]).to eq(DateTime.new(2021,8,15))
-      expect(output[:data].last[:sender_id]).to eq(sender_a_id)
-      expect(output[:data].last[:created_at]).to eq(DateTime.new(2021,7,01))
+      expect(output[:data].first[:attributes][:sender_id]).to eq(@sender_a_id)
+      expect(output[:data].first[:attributes][:created_at]).to eq("2021-08-15T00:00:00.000Z")
+      expect(output[:data].last[:attributes][:sender_id]).to eq(@sender_a_id)
+      expect(output[:data].last[:attributes][:created_at]).to eq("2021-05-01T00:00:00.000Z")
     end
-    
+
     it 'returns recipient\'s last 30 days of messages from a given sender'
     it 'returns recipient\'s last 100 messages from all senders if non specified'
     it 'returns recipient\'s last 30 days of messages from all senders if non specified'
