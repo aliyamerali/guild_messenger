36ec0ceb836bfdab82b0de7fc9f06d94d0c44fec
Aliya Merali <merali4@gmail.com>
2021-08-26 18:14:44 -0600

    Add sad path response functionality to messages#index

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -4,16 +4,15 @@ class Api::V1::MessagesController < ApplicationController
     sender_id = params[:sender_id]
     limit = params[:limit]
 
-    if sender_id
+    if sender_id && valid_recipient_id?(recipient_id) && valid_limit?(limit)
       messages = Message.from_sender(recipient_id, sender_id, limit)
-    else
+      render json: MessageSerializer.new(messages)
+    elsif valid_recipient_id?(recipient_id) && valid_limit?(limit)
       messages = Message.from_all_senders(recipient_id, limit)
+      render json: MessageSerializer.new(messages)
+    else
+      render json: { response: 'Bad Request'}, status: :bad_request
     end
-
-    render json: MessageSerializer.new(messages)
-    # else
-    #   render json: { response: 'Bad Request'}, status: :bad_request
-    # end
   end
 
   def create
@@ -29,5 +28,13 @@ class Api::V1::MessagesController < ApplicationController
 
   def message_params
     params.permit(:sender_id, :recipient_id, :content)
+  end
+
+  def valid_limit?(limit)
+    limit == "100" || "30d"
+  end
+
+  def valid_recipient_id?(recipient_id)
+    recipient_id.to_i != 0
   end
 end
